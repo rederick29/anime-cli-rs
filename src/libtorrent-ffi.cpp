29082c9c68a1004143ff9313ad40d86b52e3f59d
Erick Vilcica <erick.vilcica@protonmail.com>
2022-07-30 16:02:52 +0100

    Improve UI while downloading and change libtorrent settings

--- src/libtorrent-ffi.cpp
+++ src/libtorrent-ffi.cpp
@@ -1,9 +1,10 @@
 #include "libtorrent-ffi.hpp"
 #include <chrono>
-#include <cstdio>
-#include <stdexcept>
-#include <thread>
+#include <cmath>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <thread>
 
 // Download provided magnet link.
 // TODO: This currently leeches, should rather
@@ -13,12 +14,25 @@ char* download_magnet(const char* magnet) {
     try {
         // Start new libtorrent session
         lt::session session;
+        std::cout << "Starting libtorrent version " << lt::version() << std::endl;
+
+        // Limit connections and disable uTP
+        lt::settings_pack settings = lt::default_settings();
+        settings.set_int(lt::settings_pack::connections_limit, 100);
+        settings.set_bool(lt::settings_pack::enable_outgoing_utp, false);
+        settings.set_bool(lt::settings_pack::enable_incoming_utp, false);
+        settings.set_int(lt::settings_pack::enc_policy(), 0); // unsure if actually works
+        settings.set_int(lt::settings_pack::enc_level(), 3);  // unsure if actually works
+        session.apply_settings(settings);
+
         // Get metadata from magnet
         lt::add_torrent_params atp = lt::parse_magnet_uri(magnet);
         // Set temporary save path
         atp.save_path = "/tmp/";
         // Initialise torrent
         lt::torrent_handle t_handle = session.add_torrent(atp);
+        // Make sequential for video playing WIP
+        // t_handle.set_flags(lt::torrent_flags::sequential_download);
 
         // Wait for metadata to be retrieved
         std::cout << "Awaiting torrent metadata..." << std::endl;
@@ -45,12 +59,16 @@ char* download_magnet(const char* magnet) {
         // Save path of first file - TODO: Don't just pick the first file
         std::string file_path = t_status.save_path + t_files.file_path(0);
 
+        std::string states[] = { "Checking Files", "Downloading Metadata", "Downloading", "Finished",
+                                 "Seeding", "Allocating", "Checking Resume Data" };
+
         // Until it finishes downloading, poll status and write out percentage every 2s
         bool finished = t_status.is_finished;
         while (!finished) {
             t_status = t_handle.status();
-            std::cout << "\r";
-            printf("%.1f%% complete", t_status.progress*100);
+            std::cout << "\r" << states[t_status.state-1] << " " << std::round(t_status.progress*100)
+                      << "% " << t_status.download_rate/1000 << " KB/s down " << t_status.upload_rate/1000
+                      << " KB/s up Peers:" << t_status.num_peers << "       "; // empty space for ovewriting any characters left
             std::cout << std::flush;
             finished = t_status.is_finished;
             std::this_thread::sleep_for(std::chrono::seconds(2));
@@ -61,7 +79,6 @@ char* download_magnet(const char* magnet) {
         // Make new file path string to return to rust
         char* output_path = (char*) malloc(file_path.length()+1);
         std::strncpy(output_path, file_path.c_str(), file_path.length()+1);
-        // std::cout << "[src/libtorrent-ffi.cpp:62] output_path = " << output_path << std::endl; //debug
         if (output_path == nullptr) {
             throw std::runtime_error("c++ output_path is nullptr");
         } else {
--- src/libtorrent-ffi.hpp
+++ src/libtorrent-ffi.hpp
@@ -1,12 +1,14 @@
 #ifndef LIBTORRENT_FFI_H
 #define LIBTORRENT_FFI_H
-#endif 
-#include <libtorrent/session.hpp>
-#include <libtorrent/add_torrent_params.hpp>
-#include <libtorrent/torrent_handle.hpp>
-#include <libtorrent/magnet_uri.hpp>
+#endif
+#include "libtorrent/session.hpp"
+#include "libtorrent/add_torrent_params.hpp"
+#include "libtorrent/torrent_handle.hpp"
+#include "libtorrent/magnet_uri.hpp"
 #include "libtorrent/torrent_info.hpp"
 #include "libtorrent/torrent_status.hpp"
+#include "libtorrent/settings_pack.hpp"
+#include "libtorrent/torrent_flags.hpp"
 
 // Download given magnet link, returning path to first file downloaded
 char* download_magnet(const char* magnet);
